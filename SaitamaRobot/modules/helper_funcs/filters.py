1786517d686ed8b175aba7e9e3cecf8755ee5c34
Flash11553 <flash.14599@gmail.com>
2025-07-30 17:45:29 +0400

    Update filters.py

--- SaitamaRobot/modules/helper_funcs/filters.py
+++ SaitamaRobot/modules/helper_funcs/filters.py
@@ -1,41 +1,44 @@
+from telegram import Update, Message
+from telegram.ext import MessageFilter
 from SaitamaRobot import DEV_USERS, DRAGONS, DEMONS
-from telegram import Message
-from telegram.ext import BaseFilter
 
+class CustomFilters:
 
-class CustomFilters(object):
-
-    class _Supporters(BaseFilter):
-        def __call__(self, message: Message) -> bool:
+    class _Supporters(MessageFilter):
+        def filter(self, update: Update) -> bool:
+            message: Message = update.effective_message
             return bool(message.from_user and message.from_user.id in DEMONS)
 
     support_filter = _Supporters()
 
-    class _Sudoers(BaseFilter):
-        def __call__(self, message: Message) -> bool:
+    class _Sudoers(MessageFilter):
+        def filter(self, update: Update) -> bool:
+            message: Message = update.effective_message
             return bool(message.from_user and message.from_user.id in DRAGONS)
 
     sudo_filter = _Sudoers()
 
-    class _Developers(BaseFilter):
-        def __call__(self, message: Message) -> bool:
+    class _Developers(MessageFilter):
+        def filter(self, update: Update) -> bool:
+            message: Message = update.effective_message
             return bool(message.from_user and message.from_user.id in DEV_USERS)
 
     dev_filter = _Developers()
 
-    class _MimeType(BaseFilter):
+    class _MimeType(MessageFilter):
         def __init__(self, mimetype: str):
             self.mime_type = mimetype
             self.name = f"CustomFilters.mime_type({self.mime_type})"
 
-        def __call__(self, message: Message) -> bool:
-            return bool(message.document and
-                        message.document.mime_type == self.mime_type)
+        def filter(self, update: Update) -> bool:
+            message: Message = update.effective_message
+            return bool(message.document and message.document.mime_type == self.mime_type)
 
     mime_type = _MimeType
 
-    class _HasText(BaseFilter):
-        def __call__(self, message: Message) -> bool:
+    class _HasText(MessageFilter):
+        def filter(self, update: Update) -> bool:
+            message: Message = update.effective_message
             return bool(
                 message.text or message.sticker or message.photo or
                 message.document or message.video
