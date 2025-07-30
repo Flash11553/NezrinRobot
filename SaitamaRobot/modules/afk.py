7f818386751290b0d5bc63bee57269102087d76d
Flash11553 <flash.14599@gmail.com>
2025-07-30 17:04:45 +0400

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -142,7 +142,7 @@ Siz afk olanda kimsə sizi tag etsə, reply atsa bot ona cavab verəcəy
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
 AFK_REGEX_HANDLER = DisableAbleMessageHandler(
-    Filters.regex(r"^(?i)afk(.*)$"), afk, friendly="afk")
+    Filters.regex(r"(?i)^afk(.*)$"), afk, friendly="afk")
 NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk)
 AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group, reply_afk)
 
