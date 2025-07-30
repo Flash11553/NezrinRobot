0f98c8bedc8ee4bcb92dbb2586360c0d44a8affe
Flash11553 <flash.14599@gmail.com>
2025-07-30 17:55:54 +0400

    Update filters.py

--- SaitamaRobot/modules/helper_funcs/filters.py
+++ SaitamaRobot/modules/helper_funcs/filters.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import MessageFilter
 from SaitamaRobot import DEV_USERS, DRAGONS, DEMONS
 
+
 class CustomFilters:
 
     class _Supporters(MessageFilter):
@@ -31,19 +32,22 @@ class CustomFilters:
             self.name = f"CustomFilters.mime_type({self.mime_type})"
 
         def filter(self, update: Update) -> bool:
-            msg = update.effective_message
-            return bool(msg.document and msg.document.mime_type == self.mime_type)
+            msg = getattr(update, "effective_message", None)
+            return bool(msg and msg.document and msg.document.mime_type == self.mime_type)
 
     mime_type = _MimeType
 
     class _HasText(MessageFilter):
         def filter(self, update: Update) -> bool:
-            msg = update.effective_message
-            return bool(
-                msg and (
-                    msg.text or msg.sticker or msg.photo or
-                    msg.document or msg.video
-                )
-            )
+            msg = getattr(update, "effective_message", None)
+            if not msg:
+                return False
+            return any([
+                getattr(msg, "text", None),
+                getattr(msg, "sticker", None),
+                getattr(msg, "photo", None),
+                getattr(msg, "document", None),
+                getattr(msg, "video", None),
+            ])
 
     has_text = _HasText()
